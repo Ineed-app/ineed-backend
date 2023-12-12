311cc7bfec6eb78394d6e6009324e9c2ba5f5eb0
Sai Reddy <startsup.getin@gmail.com>
2023-12-12 14:14:08 +0530

    update
    update

--- app.js
+++ app.js
@@ -5,9 +5,12 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-app.get('/chattest', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+app.get('/', (req, res) => {
+    return res.status(400).json({ message: "working" });
 });
+// app.get('/chattest', (req, res) => {
+//     res.sendFile(__dirname + '/index.html');
+// });
 
 io.on('connection', (socket) => {
     console.log('a user connected');
