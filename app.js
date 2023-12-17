d5a39fe32e475eb06acae8e66c35eaf41fc22584
Sai Reddy <startsup.getin@gmail.com>
2023-12-17 21:11:18 +0530

    update
    update

--- app.js
+++ app.js
@@ -63,6 +63,13 @@ io.on('connection', (socket) => {
         messagescontroller.send_message(data);
         io.emit('receivemessage', data);
     });
+
+    socket.on('loadmessages', (data) => {
+        console.log(data)
+        const result = messagescontroller.getallmessages(data)
+        console.log(result)
+        io.emit('OnLoadMessages', result)
+    })
 });
 
 server.listen(3000, () => {
--- controllers/messages.js
+++ controllers/messages.js
@@ -26,3 +26,24 @@ exports.send_message = async(data) => {
         return;
     }
 };
+
+exports.getallmessages = async(data) => {
+    console.log("i am in controller", data);
+
+    const messages = await Message.find();
+
+    var res_messages = [];
+
+    for (let index = 0; index < messages.length; index++) {
+        const element = messages[index];
+
+        if (element.sender_id == data.sender_id && element.receiver_id == data.receiver_id)
+            res_messages.push(element)
+        else if (element.sender_id == data.receiver_id && element.receiver_id == data.sender_id)
+            res_messages.push(element)
+    }
+
+    return res_messages;
+
+
+}
